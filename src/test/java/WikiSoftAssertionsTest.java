88de00779a3a038950a29e45fa568d86b85520c2
daniil.sosnovsky <dsosnovsky@aquivalabs.com>
2024-04-25 21:50:41 -0300

    Untracked .idea and fix for test

--- src/test/java/WikiSoftAssertionsTest.java
+++ src/test/java/WikiSoftAssertionsTest.java
@@ -4,12 +4,9 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.time.Duration;
-
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
-import static java.time.Duration.ofSeconds;
 
 public class WikiSoftAssertionsTest {
 
@@ -24,16 +21,26 @@ public class WikiSoftAssertionsTest {
     void junitExampleExistOnWiki() {
         open("/selenide/selenide");
 
-        $("#wiki-tab").shouldBe(visible, ofSeconds(10)).click();
-        $("#wiki-pages-filter").shouldBe(visible, ofSeconds(10)).setValue("SoftAssertions");
-        $(byText("SoftAssertions")).shouldBe(visible, ofSeconds(10)).click();
+        $("#wiki-tab").shouldBe(visible).click();
+        $("#wiki-pages-filter").shouldBe(visible).setValue("SoftAssertions");
+        $(byText("SoftAssertions")).shouldBe(visible).click();
         $$(".heading-element")
                 .filter(text("JUnit5"))
                 .first()
                 .parent()
                 .sibling(0)
-                .$("pre")
-                .shouldBe(visible, ofSeconds(10));
+                .shouldHave(text("""
+            @ExtendWith({SoftAssertsExtension.class})
+            class Tests {
+                @Test
+                void test() {
+                    Configuration.assertionMode = SOFT;
+                    open("page.html");
+
+                    $("#first").should(visible).click();
+                    $("#second").should(visible).click();
+                }
+            }"""));
     }
 
     @AfterAll
